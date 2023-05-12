ecb46e5cbdd9c8037cdeb7d55b04cad243142f28
nozomi-koborinai <kobofender@gmail.com>
2023-05-12 12:57:46 +0900

    add helloGet HTTP Cloud Function

---
+++ helloworld/hello_world.go
@@ -0,0 +1,17 @@
+package helloworld
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("HelloGet", helloGet)
+}
+
+// helloGet is an HTTP Cloud Function.
+func helloGet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
